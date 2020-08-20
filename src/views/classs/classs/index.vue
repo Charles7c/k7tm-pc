21538dd21a8b670fabf34d2eb7129a8e95de8f13
MrLi <642177588@qq.com>
2020-08-20 22:43:03 +0800

    refactor(src/views/classs/classs): :recycle:调整了我的班级
    - 缺少部分字典 后期调整

--- src/views/classs/classs/index.vue
+++ src/views/classs/classs/index.vue
@@ -27,26 +27,91 @@
             :value="item.value"
           />
         </el-select>
-        <label class="el-form-item-label">状态</label>
-        <el-input v-model="query.status" clearable placeholder="状态" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
-        <label class="el-form-item-label">课程类型</label>
-        <el-input v-model="query.courseId" clearable placeholder="课程类型" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
-        <label class="el-form-item-label">职业顾问</label>
-        <el-input v-model="query.vcTeacher" clearable placeholder="职业顾问" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
-        <label class="el-form-item-label">技术顾问</label>
-        <el-input v-model="query.taTeacher" clearable placeholder="技术顾问" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
-        <label class="el-form-item-label">项目经理</label>
-        <el-input v-model="query.pmTeacher" clearable placeholder="项目经理" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
+        <el-select
+          v-model="query.status"
+          clearable
+          placeholder="班级状态搜索"
+          class="filter-item"
+          style="width: 134px"
+          @change="crud.toQuery"
+        >
+          <el-option
+            v-for="(item, index) in dict.classs_status"
+            :key="item.label + index"
+            :label="item.label"
+            :value="item.value"
+          />
+        </el-select>
+        <el-select
+          v-model="query.courseId"
+          clearable
+          placeholder="课程类型搜索"
+          class="filter-item"
+          style="width: 134px"
+          @change="crud.toQuery"
+        >
+          <el-option
+            v-for="(item, index) in base.course"
+            :key="item.name + index"
+            :label="item.name"
+            :value="item.courseId"
+          />
+        </el-select>
+        <el-select
+          v-model="query.vcTeacher"
+          clearable
+          placeholder="职业顾问搜索"
+          class="filter-item"
+          style="width: 134px"
+          @change="crud.toQuery"
+        >
+          <el-option
+            v-for="(item, index) in vcTeachers"
+            :key="item.nickName + index"
+            :label="item.nickName"
+            :value="item.id"
+          />
+        </el-select>
+        <el-select
+          v-model="query.taTeacher"
+          clearable
+          placeholder="技术顾问搜索"
+          class="filter-item"
+          style="width: 134px"
+          @change="crud.toQuery"
+        >
+          <el-option
+            v-for="(item, index) in taTeachers"
+            :key="item.nickName + index"
+            :label="item.nickName"
+            :value="item.id"
+          />
+        </el-select>
+        <el-select
+          v-model="query.pmTeacher"
+          clearable
+          placeholder="项目经理搜索"
+          class="filter-item"
+          style="width: 134px"
+          @change="crud.toQuery"
+        >
+          <el-option
+            v-for="(item, index) in pmTeachers"
+            :key="item.nickName + index"
+            :label="item.nickName"
+            :value="item.id"
+          />
+        </el-select>
         <date-range-picker
           v-model="query.classTime"
-          start-placeholder="classTimeStart"
-          end-placeholder="classTimeStart"
+          start-placeholder="开班时间起"
+          end-placeholder="开班时间止"
           class="date-item"
         />
         <date-range-picker
           v-model="query.graduateTime"
-          start-placeholder="graduateTimeStart"
-          end-placeholder="graduateTimeStart"
+          start-placeholder="毕业时间起"
+          end-placeholder="毕业时间止"
           class="date-item"
         />
         <rrOperation :crud="crud" />
@@ -69,11 +134,25 @@
               />
             </el-select>
           </el-form-item>
-          <el-form-item label="状态">
-            未设置字典，请手动设置 Select
+          <el-form-item label="状态" prop="status">
+            <el-select v-model="form.status" clearable style="width: 194px">
+              <el-option
+                v-for="(item, index) in dict.classs_status"
+                :key="item.label + index"
+                :label="item.label"
+                :value="parseInt(item.value)"
+              />
+            </el-select>
           </el-form-item>
-          <el-form-item label="课程类型">
-            未设置字典，请手动设置 Select
+          <el-form-item label="课程类型" prop="course">
+            <el-select v-model="form.course" clearable style="width: 194px">
+              <el-option
+                v-for="(item, index) in base.course"
+                :key="item.name + index"
+                :label="item.name"
+                :value="item.courseId"
+              />
+            </el-select>
           </el-form-item>
           <el-form-item label="职业顾问" prop="vcTeacher">
             未设置字典，请手动设置 Select
@@ -97,7 +176,8 @@
               type="textarea"
               :autosize="{minRows:3}"
               placeholder="班级的情况都可以写在这里"
-              :style="'width:'+textareaStyle"/>
+              :style="'width:'+textareaStyle"
+            />
           </el-form-item>
         </el-form>
         <div slot="footer" class="dialog-footer" align="center">
@@ -108,29 +188,37 @@
       <!--表格渲染-->
       <el-table ref="table" v-loading="crud.loading" :data="crud.data" size="small" style="width: 100%;" @selection-change="crud.selectionChangeHandler">
         <el-table-column type="selection" width="55" />
-        <el-table-column prop="name" label="名称" align="center"/>
+        <el-table-column prop="name" label="名称" align="center" />
         <el-table-column prop="type" label="类型" align="center">
           <template slot-scope="scope">
             {{ dict.label.classs_type[scope.row.type] }}
           </template>
         </el-table-column>
-        <el-table-column prop="status" label="状态" />
-        <el-table-column prop="stuNum" label="现有人数" />
-        <el-table-column prop="classTransferNum" label="转班人数" />
-        <el-table-column prop="temporaryAbsenceNum" label="休学人数" />
-        <el-table-column prop="refundNum" label="退学人数" />
-        <el-table-column prop="expelNum" label="开除人数" />
-        <el-table-column prop="courseId" label="课程类型" />
-        <el-table-column prop="vcTeacher" label="职业顾问" />
-        <el-table-column prop="taTeacher" label="技术顾问" />
-        <el-table-column prop="pmTeacher" label="项目经理" />
+        <el-table-column prop="status" label="状态" align="center">
+          <template slot-scope="scope">
+            {{ dict.label.classs_status[scope.row.status] }}
+          </template>
+        </el-table-column>
+        <el-table-column prop="stuNum" label="现有人数" align="center" />
+        <el-table-column prop="classTransferNum" label="转班人数" align="center" />
+        <el-table-column prop="temporaryAbsenceNum" label="休学人数" align="center" />
+        <el-table-column prop="refundNum" label="退学人数" align="center" />
+        <el-table-column prop="expelNum" label="开除人数" align="center" />
+        <el-table-column prop="courseId" label="课程类型" width="120" align="center">
+          <template slot-scope="scope">
+            {{ base.name.course[scope.row.courseId] }}
+          </template>
+        </el-table-column>
+        <el-table-column prop="vcTeacher" label="职业顾问" align="center" />
+        <el-table-column prop="taTeacher" label="技术顾问" align="center" />
+        <el-table-column prop="pmTeacher" label="项目经理" align="center" />
         <el-table-column prop="classTime" label="开班时间" width="120" :formatter="formatDate" align="center" />
         <el-table-column prop="graduateTime" label="毕业时间" width="120" :formatter="formatDate" align="center">
           <template slot-scope="scope">
             <span>{{ parseTime(scope.row.graduateTime) }}</span>
           </template>
         </el-table-column>
-        <el-table-column prop="updateBy" label="更新者" />
+        <el-table-column prop="updateBy" label="更新者" align="center" />
         <el-table-column prop="updateTime" label="更新时间" width="135" align="center">
           <template slot-scope="scope">
             <span>{{ parseTime(scope.row.updateTime) }}</span>
@@ -158,20 +246,22 @@ import rrOperation from '@crud/RR.operation'
 import crudOperation from '@crud/CRUD.operation'
 import udOperation from '@crud/UD.operation'
 import pagination from '@crud/Pagination'
+import DateRangePicker from '@/components/DateRangePicker'
 import { checkDevice } from '@/utils/index'
 
-const defaultForm = { classId: null, name: null, type: null, status: null, stuNum: null, classTransferNum: null, temporaryAbsenceNum: null, refundNum: null, expelNum: null, courseId: null, vcTeacher: null, taTeacher: null, pmTeacher: null, classTime: null, graduateTime: null, desc: null, createBy: null, updateBy: null, createTime: null, updateTime: null}
+const defaultForm = { classId: null, name: null, type: null, status: null, stuNum: null, classTransferNum: null, temporaryAbsenceNum: null, refundNum: null, expelNum: null, courseId: null, vcTeacher: null, taTeacher: null, pmTeacher: null, classTime: null, graduateTime: null, desc: null, createBy: null, updateBy: null, createTime: null, updateTime: null }
 export default {
   name: 'Classs',
-  components: { pagination, crudOperation, rrOperation, udOperation },
+  components: { pagination, crudOperation, rrOperation, udOperation, DateRangePicker },
   mixins: [presenter(), header(), form(defaultForm), crud()],
-  dicts: ['classs_type'],
+  dicts: ['classs_type', 'classs_status'],
+  bases: ['course'],
   cruds() {
     return CRUD({ title: '班级', url: 'api/classs', sort: 'classId,desc', crudMethod: { ...crudClasss }})
   },
   data() {
     return {
-      textareaStyle: '270%', dialogStyle: '660px',
+      textareaStyle: '260%', dialogStyle: '660px',
       permission: {
         add: ['admin', 'classs:add'],
         edit: ['admin', 'classs:edit'],
@@ -179,19 +269,32 @@ export default {
       },
       rules: {
         name: [
-          { required: true, message: '名称不能为空', trigger: 'blur' }
+          { required: true, message: '请输入班级名称', trigger: 'blur' },
+          { min: 2, max: 15, message: '班级名称应在 2 到 15 个字符' }
         ],
         type: [
-          { required: true, message: '请选择类型', trigger: 'blur' }
+          { required: true, message: '请选择班级类型', trigger: 'blur' }
         ],
-        vcTeacher: [
-          { required: true, message: '职业顾问不能为空', trigger: 'blur' }
+        'course.id': [
+          { required: true, message: '请选择课程类型', trigger: 'blur' }
         ],
-        name: [
-          { required: true, message: '名称不能为空', trigger: 'blur' }
+        'vcTeacher.id': [
+          { required: true, message: '请选择职业顾问', trigger: 'blur' }
         ],
-        vcTeacher: [
-          { required: true, message: '职业顾问不能为空', trigger: 'blur' }
+        'taTeacher.id': [
+          { required: true, message: '请选择技术顾问', trigger: 'blur' }
+        ],
+        'pmTeacher.id': [
+          { required: true, message: '请选择项目经理', trigger: 'blur' }
+        ],
+        status: [
+          { required: true, message: '请选择班级状态', trigger: 'blur' }
+        ],
+        classTime: [
+          { required: true, message: '请选择开班时间', trigger: 'blur' }
+        ],
+        stuClassDesc: [
+          { max: 200, message: '班级描述请控制在200个字符内' }
         ]
       },
       queryTypeOptions: [
